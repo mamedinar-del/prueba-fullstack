7f64c602dccbd6b74de8eef2713c995e9ea9e2bb
mamedinar-del <ma.medinar@duocuc.cl>
2025-12-16 02:43:15 -0300

    Se implementa covertura de Testing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+
   base: "/prueba-fullstack", 
 
   test: {
@@ -12,5 +12,10 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
     css: true,
+    
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
   },
 })
